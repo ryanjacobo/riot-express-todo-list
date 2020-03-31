09a33aada620587fd20b3f0bb91487cbf13d44bc
Ryan Jacobo <ryanjacobo@yahoo.com>
2020-03-31 13:30:59 -0700

    installed node_modules and updated .connect code

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.84.57.9',
+      host: '54.242.2.197',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
