cbf70002200cd77f47b52a7690bba35988030a2d
Ryan Jacobo <ryanjacobo@yahoo.com>
2020-03-31 13:27:37 -0700

    installed node_modules and changed privateKey

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '3.84.57.9',
       username: 'ubuntu',
-      privateKey: 'hs-key2.pem'
+      privateKey: 'hs-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
